4200684dc22b2820c37af0514ae113bc7ce8e06c
Chris Daley <cjdaleyb@gmail.com>
2019-02-21 20:09:16 -0500

    Return non-development environment scripts. Add support for area routing.

--- JsNamespace/Startup.cs
+++ JsNamespace/Startup.cs
@@ -38,12 +38,9 @@ namespace JsNamespace
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                  name: "areas",
-                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-            });
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
-            app.UseMvc(routes =>
-            {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
